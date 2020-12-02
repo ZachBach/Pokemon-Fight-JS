3227f4885cf2d98da1612ece1e5ae27a65519135
Zach Auerbach <auernachzach@gmail.com>
2020-12-01 17:46:57 -0800

    added pokemon arr of objs and began populating the obj with available pokemon

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 var gameMusic = {};
 spriteType = '';
 types = [];
-gameData = {};
+gameState = {};
 attackName = '';
 currAttack = {};
 randNumber = 0;
 enemyAttack = {};
-characters = [];
+pokeMon = [];
 defendProgressInt = null;
 defendProgressComplete = 0;
 progressInt = null;
@@ -32,8 +32,10 @@ populateVariables = () => {
     bulbasaur:
       'http://66.90.91.26/ost/pokemon-gameboy-sound-collection/gvqmhhryki/146-bulbasaur.mp3',
   };
+
   typeSprite =
     'http://orig15.deviantart.net/24d8/f/2011/057/3/5/ge___energy_type_icons_by_aschefield101-d3agp02.png';
+
   types = [
     'bug',
     'dark',
@@ -53,5 +55,78 @@ populateVariables = () => {
     'rock',
     'steel',
     'water',
+  ];
+
+  // Initialize the state of the game
+  gameState = {
+    step: 1,
+    hero: {},
+    enemy: {},
+  };
+
+  //   Attack Variables
+  attackName = '';
+  currAttack = {};
+  randNumber = 0;
+  enemyAttack = {};
+  defendProgressInt = null;
+  defendProgressComplete = 0;
+  progressInt = null;
+  progressComplete = 0;
+
+  // Avaiable Pokemon
+  pokeMon = [
+    {
+      name: 'pikachu',
+      type: 'electric',
+      weakness: ['fighting'],
+      resistance: ['steel'],
+      img: {
+        default:
+          'http://vignette2.wikia.nocookie.net/all-anime-characters/images/7/7b/Cute_pikachu_with_hat_by_mlpochea-d63xlom.png/revision/latest?cb=20150108111832',
+        front:
+          'http://rs1263.pbsrc.com/albums/ii631/Pokemon-Vampire-Knight-lover/pikachu_.gif~c200',
+        back:
+          'http://vignette4.wikia.nocookie.net/pokemon/images/5/5b/Pikachu_Back_XY.gif/revision/latest?cb=20141009080948',
+      },
+      hp: {
+        current: 500,
+        total: 500,
+      },
+      attacks: [
+        {
+          name: 'thunder bolt',
+          hp: randomNum(40, 20),
+          avail: {
+            total: 30,
+            remaining: 30,
+          },
+        },
+        {
+          name: 'quick attack',
+          hp: randNumber(30, 15),
+          avail: {
+            total: 15,
+            remaining: 15,
+          },
+        },
+        {
+          name: 'lightning',
+          hp: randNumber(100, 50),
+          avail: {
+            total: 5,
+            remaining: 5,
+          },
+        },
+        {
+          name: 'headbutt',
+          hp: randNumber(60, 40),
+          avail: {
+            total: 20,
+            remaining: 20,
+          },
+        },
+      ],
+    },
   ];
 };
